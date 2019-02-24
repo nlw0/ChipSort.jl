8fdf9396fbee7af93b75ad1a9d6243c29d3196f6
nic <nwerneck@gmail.com>
2019-02-24 13:42:05 +0100

    tests for sorting

---
+++ test/test-sorting-networks.jl
@@ -0,0 +1,61 @@
+using Test
+
+include("../src/sorting-networks.jl")
+
+function test_sorting_networks(T, N)
+    vv = rand(T, N)
+    srtref = sort(vv)
+    srthat = sort_net(vv...)
+
+    @test all(srthat[2:end] .>= srthat[1:end-1])
+    @test all(srthat .== srtref)
+end
+
+@testset for T in [Int8, Int16, Int32, Int64, Float32, Float64]
+    for L in 2 .^ (2:5)
+        test_sorting_networks(T, L)
+    end
+end
+
+
+function test_sort_simd_vec(T, N)
+    va = ntuple(n->Vec(tuple(rand(T, N)...)),N)
+    srthat = sort_net(va...)
+
+    xx = [va[j][k] for j in 1:N, k in 1:N]
+    srtref = sort(xx; dims=1)
+    srthat_arr = [srthat[j][k] for j in 1:N, k in 1:N]
+
+    @test srthat_arr == srtref
+end
+
+@testset for T in [Int8, Int16, Int32, Int64, Float32, Float64]
+    Niterations = 16
+    for L in 2 .^ (2:5)
+        for it in 1:Niterations
+            test_sort_simd_vec(T, L)
+        end
+    end
+end
+
+@testset for N in [16, 32]
+    for it in 1:2^16
+        test_sort_simd_vec(UInt32, N)
+    end
+end
+
+
+function test_sort_heaviside(N, Na)
+    # va = Vec(ntuple(n->if n<=Na 0x0 else 0x1 end, N))
+    va = ntuple(n->if (Na&(1<<(n-1))) > 0 1 else 0 end, N)
+    srthat = sort_net(va...)
+    srtref = sort([va...])
+    @test srthat == tuple(srtref...)
+end
+
+
+@testset for N in 2 .^ (2:4)
+    for Na in 0:(2^N-1)
+            test_sort_heaviside(N, Na)
+    end
+end
--- test/test-transpose.jl
+++ test/test-transpose.jl
@@ -1,20 +1,20 @@
 using Test
 
-# using MergeSortSIMD
+using MergeSortSIMD
 using SIMD
 
-include("../src/matrix-transpose.jl")
 
-function test_transpose(T, L)
+function test_transpose_vecs(T, L)
     uu = ntuple(a->Vec(ntuple(i->convert(T, mod(a*(L)+i, 2^(8*T.size-2))), L)), L)
     tt = ntuple(a->Vec(ntuple(i->convert(T, mod(i*(L)+a, 2^(8*T.size-2))), L)), L)
-    ee = transpose_vecs(uu)
+    ee = transpose_vecs(uu...)
     tthat = eval(ee)
     @test all(all(j) for j in (tthat.==tt))
 end
 
+
 @testset for T in [UInt8, UInt16, UInt32, UInt64, Float32, Float64]
     for L in 2 .^ (1:4)
-        transpose_vecs(T, L)
+        test_transpose_vecs(T, L)
     end
 end
