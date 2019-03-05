3f2e9e9bef0f757fd4ac4713635a3969ca7f2406
nic <nwerneck@gmail.com>
2019-03-05 21:11:24 +0100

    new documentation

--- src/sorting-networks.jl
+++ src/sorting-networks.jl
@@ -2,6 +2,14 @@ using SIMD
 
 include("sorting-network-parameters.jl")
 
+
+"""
+    sort_net(input::Vararg{T, L}) where {L,T}
+
+Applies a sorting network of size L to the input elements, returning a sorted tuple.
+
+The elements must support the `min` and `max` functions. In the case of `SIMD.Vec` objects each "lane" across the vectors will be sorted. Therefore with L vectors of size N this function will produce N sorted sequences of size L, after the data is transposed (see transpose_vecs).
+"""
 @generated function sort_net(input::Vararg{T, L}) where {L,T}
 
     ex = Expr[Expr(:meta, :inline)]
