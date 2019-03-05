692a8ccd3dfdb7ef1f7ad4fb7f9c75324eb8395d
Nicolau Werneck <nwerneck@gmail.com>
2019-03-05 22:49:24 +0100

    Update bitonic-merge-network.jl

--- src/bitonic-merge-network.jl
+++ src/bitonic-merge-network.jl
@@ -22,7 +22,7 @@ end
 """
     bitonic_merge(input_a::Vec{N,T}, input_b::Vec{N,T}) where {N,T}
 
-Merges two `SIMD.Vec` objects of the same type and size using a bitonic sort network. The inputs are assumed to be sorted. Returns a pair of vectors wich the merged sequence.
+Merges two `SIMD.Vec` objects of the same type and size using a bitonic sort network. The inputs are assumed to be sorted. Returns a pair of vectors with the first and second halves of the merged sequence.
 """
 @generated function bitonic_merge(input_a::Vec{N,T}, input_b::Vec{N,T}) where {N,T}
 
