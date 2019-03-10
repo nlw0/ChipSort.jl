d964ba908c67b6684071df6244f51441748dda1d
nic <nwerneck@gmail.com>
2019-03-10 21:45:56 +0100

    Fixing tests

--- test/test-chipsort.jl
+++ test/test-chipsort.jl
@@ -2,12 +2,17 @@ using Test
 using ChipSort
 using SIMD
 
+
+@testset "Array sorting"
+
 data = rand(256)
 @test chipsort(data,Val(8),Val(8),Val(8)) == sort(data)
 
 data = rand(256)
-@test chipsort_medium(data,Val(8),Val(8)) == sort(data)
+@test chipsort_medium(data,Val(4),Val(8),Val(8)) == sort(data)
 
 data = tuple((Vec(tuple(sort(rand(Int8,4))...)) for _ in 1:4)...)
 stream_to_array(data) = [k[i] for i in 1:length(data), k in data][:]
 @test stream_to_array(merge_vecs_tree(data...)) == sort(stream_to_array(data))
+
+end
