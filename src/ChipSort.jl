640f88f9d7d69ff1e88fe1e3aa70995d97c8ce4f
nic <nwerneck@gmail.com>
2019-03-10 17:50:58 +0100

    Test for stream merging function

--- src/ChipSort.jl
+++ src/ChipSort.jl
@@ -6,7 +6,7 @@ export
     transpose_vecs, transpose_vecs_tall, transpose_vecs_wide,
     bitonic_merge, merge_vecs, build_multi_merger,
     DataBuffer, MergeNode, pop!,
-    chipsort, sort_chunks, chipsort_medium
+    chipsort, sort_chunks, chipsort_medium, merge_vecs_tree
 
 
 include("utils.jl")
--- test/test-chipsort.jl
+++ test/test-chipsort.jl
@@ -1,9 +1,13 @@
 using Test
 using ChipSort
-
+using SIMD
 
 data = rand(256)
 @test chipsort(data,Val(8),Val(8),Val(8)) == sort(data)
 
 data = rand(256)
 @test chipsort_medium(data,Val(8),Val(8)) == sort(data)
+
+data = tuple((Vec(tuple(sort(rand(Int8,4))...)) for _ in 1:4)...)
+stream_to_array(data) = [k[i] for i in 1:length(data), k in data][:]
+@test stream_to_array(merge_vecs_tree(data...)) == sort(stream_to_array(data))
